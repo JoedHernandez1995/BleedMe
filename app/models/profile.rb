b3932189e59c6b14b05845b7a48c9fa28e9d6d73
JoedHernandez1995 <joedhernandez95@unitec.edu>
2016-02-10 21:37:18 -0600

    add pictures

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,11 @@
 class Profile < ActiveRecord::Base
-  has_attached_file :image
+  has_attached_file :image,
+                    url: "/system/:hash.:extension",
+                    hash_secret: "abc123"
   validates_attachment :image,
-                     content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+                     content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
+                     styles: { thumb: ["64x64#", :jpg] },
+                     convert_options: { thumb: "-quality 75 -strip",
+                                        original: "-quality 85 -strip" },
+                     size: {in: 0..500.kilobytes}
 end
